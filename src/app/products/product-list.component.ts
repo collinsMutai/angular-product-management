9fc11da39d91d39122a641c46aacb39b24fdcaec
collinsMutai <collinsfrontend@gmail.com>
2021-12-09 06:11:10 +0300

    define interface, OnInit hook and custom pipe

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,17 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './products';
 //  we'll use the component as a directive
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  // enscapulating component styles
+  styleUrls: ['./product-list-component.css']
 })
-export class ProductListComponent {
+// implement OnInit lifecycle hook
+export class ProductListComponent implements OnInit{
   pageTitle: string = 'Interpolation';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
+
+  // getter and setter
+  private _listFilter: string = '';
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string){
+    this._listFilter = value;
+    console.log('In setter:', value);
+    this.filteredProducts = this.performFilter(value);
+  }
+
+
+  filteredProducts: IProduct[] = []
+  
   // pass products to html template
-  products: any[] = [
+  // use as an interface
+  products: IProduct[] = [
     {
       "productId": 1,
       "productName": "Leaf Rake",
@@ -64,8 +85,20 @@ export class ProductListComponent {
     }
   ];
 
+  //  filter products
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) => 
+product.productName.toLocaleLowerCase().includes(filterBy))
+  }
+
   // toggle image method
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  // hook method for OnInit interface
+  ngOnInit(): void {
+    this._listFilter = 'cart';
   }
 }
---
+++ src/app/products/products.ts
@@ -0,0 +1,11 @@
+// defining interface
+export interface IProduct {
+    "productId": number;
+    "productName": string;
+    "productCode": string;
+    "releaseDate": string;
+    "description": string;
+    "price": number;
+    "starRating": number
+    "imageUrl": string;
+  }
