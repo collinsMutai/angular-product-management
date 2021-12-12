104b4ac20ba29bc13be4e9fa97fd4fe21a14d62d
collinsMutai <collinsfrontend@gmail.com>
2021-12-12 14:00:17 +0300

    add product data to template using a service

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from './product.service';
 import { IProduct } from './products';
 //  we'll use the component as a directive
 @Component({
@@ -32,58 +33,12 @@ export class ProductListComponent implements OnInit{
   
   // pass products to html template
   // use as an interface
-  products: IProduct[] = [
-    {
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2021",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png"
-    },
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2021",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    },
-    {
-      "productId": 8,
-      "productName": "Saw",
-      "productCode": "TBX-0022",
-      "releaseDate": "May 15, 2021",
-      "description": "15-inch steel blade hand saw",
-      "price": 11.55,
-      "starRating": 3.7,
-      "imageUrl": "assets/images/saw.png"
-    },
-    {
-      "productId": 10,
-      "productName": "Video Game Controller",
-      "productCode": "GMG-0042",
-      "releaseDate": "October 15, 2020",
-      "description": "Standard two-button video game controller",
-      "price": 35.95,
-      "starRating": 4.6,
-      "imageUrl": "assets/images/xbox-controller.png"
-    }
-  ];
+  products: IProduct[] = [ ];
+
+
+// define the dependecy and inject the service
+constructor(private productService: ProductService) {}
+
 
   //  filter products
   performFilter(filterBy: string): IProduct[] {
@@ -97,9 +52,12 @@ product.productName.toLocaleLowerCase().includes(filterBy))
     this.showImage = !this.showImage;
   }
 
-  // hook method for OnInit interface
+  // hook method for OnInit interface. Perform any component initialization eg retrieve data for the template
   ngOnInit(): void {
-    this._listFilter = 'cart';
+    // set products property to the products returned from the productService
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
+    // this._listFilter = 'cart';
   }
 
   onRatingClicked(message: string): void {
